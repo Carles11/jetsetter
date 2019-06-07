8bca1e66f91c40ddcf1617e74dced21183982596
Carles del Río Francés <cdelriofrances@Carless-iMac.local>
2019-06-07 21:50:45 +0200

    app working

--- src/components/Items.js
+++ src/components/Items.js
@@ -4,10 +4,12 @@ import Filter from './Filter';
 
 class Items extends Component {
   state = {
-    // What state does this component have?
+    searchTerm: ''
   };
 
-  updateSearchTerm = searchTerm => { };
+  updateSearchTerm = searchTerm => {
+    this.setState({ searchTerm })
+  };
 
   render() {
 
@@ -17,11 +19,11 @@ class Items extends Component {
         <h2>
           {title} ({items.length})
         </h2>
-        <Filter searchTerm={''} onChange={this.updateSearchTerm} />
+        <Filter searchTerm={this.state.searchTerm} onChange={this.updateSearchTerm} />
         {items
           .filter(item =>
             // Hmm… this needs some work.
-            item.value.toLowerCase().includes(''.toLowerCase()),
+            item.value.toLowerCase().includes(this.state.searchTerm.toLowerCase()),
           )
           .map(item => (
             <Item
