6009a7f3778f324924f561a9ad5fd340edfa6ce0
Carles del Río Francés <cdelriofrances@Carless-iMac.local>
2019-06-07 21:15:46 +0200

    first commit

--- src/components/Application.js
+++ src/components/Application.js
@@ -22,23 +22,56 @@ const defaultState = [
 
 class Application extends Component {
   state = {
-    // Set the initial state,
+    items: defaultState
   };
 
+  addItem = (item) => {
+    this.setState({ items: [item, ...this.state.items] })
+  }
+
+  removeItem = (itemToRemove) => {
+    this.setState({
+      items: this.state.items.filter(item => item.id !== itemToRemove.id)
+    })
+  }
+
+  toggleItem = (itemToToggle) => {
+    const items = this.state.items.map(item => {
+      if (item.id !== itemToToggle.id) return item;
+      return { ...itemToToggle, packed: !itemToToggle.packed }
+    })
+    this.setState({ items })
+  }
+
+  markAllUnpacked = () => {
+    const items = this.state.items.map(item => {
+      return { ...item, packed: false }
+    })
+    this.setState({ items })
+  }
   // How are we going to manipulate the state?
   // Ideally, users are going to want to add, remove,
   // and check off items, right?
 
   render() {
-    // Get the items from state
-
+    const { items } = this.state
+    const unpackedItems = items.filter(item => !item.packed)
+    const packedItems = items.filter(item => item.packed)
     return (
       <div className="Application">
-        <NewItem />
+        <NewItem onSubmit={this.addItem} />
         <CountDown />
-        <Items title="Unpacked Items" items={[]} />
-        <Items title="Packed Items" items={[]} />
-        <button className="button full-width">Mark All As Unpacked</button>
+        <Items
+          title="Unpacked Items"
+          items={unpackedItems}
+          onToggle={this.toggleItem}
+          onRemove={this.removeItem} />
+        <Items
+          title="Packed Items"
+          items={packedItems}
+          onToggle={this.toggleItem}
+          onRemove={this.removeItem} />
+        <button className="button full-width" onClick={this.markAllUnpacked}>Mark All As Unpacked</button>
       </div>
     );
   }
